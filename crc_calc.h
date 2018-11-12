d429ef6dfc32fab110404f069d830dd00958fcd2
Mitchell <mib731@uowmail.edu.au>
2018-11-11 22:03:49 -0800

    Update crc_calc.h

--- crc_calc.h
+++ crc_calc.h
@@ -1,9 +1,28 @@
 /***************************************************************************
 CRC polynomial 0xedb88320 – Contributed unknowingly by Gary S. Brown. 
-    "Copyright (C) 1986 Gary S. Brown. You may use this program, or code or tables extracted from it, as desired without restriction."
+    "Copyright (C) 1986 Gary S. Brown. You may use this program, or code or 
+    tables extracted from it, as desired without restriction."
         Paraphrased comments from the original:
-The 32 BIT ANSI X3.66 CRC checksum algorithm is used to compute the 32-bit frame check sequence in ADCCP. (ANSI X3.66, also known as FIPS PUB 71 and FED-STD-1003, the U.S. versions of CCITT's X.25 link-level protocol.)USB Device Firmware Upgrade Specification, Revision 1.1B. 1 Portable C Source for CRC and DFU SuffixThe following example source code was created by assimilating code from a number of sources. It was edited to illustrate the concepts described in this appendix.42
-USB Device Firmware Upgrade Specification, Revision 1.1The polynomial is: X^32+X^26+X^23+X^22+X^16+X^12+X^11+X^10+X^8+X^7+X^5+X^4+X^2+X^1+X^0Put the highest-order term in the lowest-order bit. The X^32 term is implied, the LSB is the X^31 term, etc. The X^0 term usually shown as +1) results in the MSB being 1. Put the highest-order term in the lowest-order bit. The X^32 term is implied, the LSB is the X^31 term, etc. The X^0 term (usually shown as +1) results in the MSB being 1.The feedback terms table consists of 256 32-bit entries. The feedback termssimply represent the results of eight shift/xor operations for all combinations of data and CRC register values. The values must be right- shifted by eight bits by the UPDCRC logic so the shift must be unsigned.
+The 32 BIT ANSI X3.66 CRC checksum algorithm is used to compute the 32-bit frame 
+check sequence in ADCCP. (ANSI X3.66, also known as FIPS PUB 71 and FED-STD-1003, 
+the U.S. versions of CCITT's X.25 link-level protocol.)
+
+USB Device Firmware Upgrade Specification, Revision 1.1B. 1 Portable C Source 
+for CRC and DFU Suffix
+The following example source code was created by assimilating code from a number 
+of sources. It was edited to illustrate the concepts described in this appendix.
+42
+
+USB Device Firmware Upgrade Specification, Revision 1.1
+The polynomial is: X^32+X^26+X^23+X^22+X^16+X^12+X^11+X^10+X^8+X^7+X^5+X^4+X^2+X^1+X^0
+Put the highest-order term in the lowest-order bit. The X^32 term is implied, the 
+LSB is the X^31 term, etc. The X^0 term usually shown as +1) results in the MSB being 1. 
+Put the highest-order term in the lowest-order bit. The X^32 term is implied, 
+the LSB is the X^31 term, etc. The X^0 term (usually shown as +1) results in the MSB being 1.
+The feedback terms table consists of 256 32-bit entries. The feedback terms
+simply represent the results of eight shift/xor operations for all combinations 
+of data and CRC register values. The values must be right- shifted by eight bits 
+by the UPDCRC logic so the shift must be unsigned.
 ***************************************************************************/
 
 #ifndef CRC_CALC_H
